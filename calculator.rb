50df40e5ba6677a2cf0e82b237f629e0b55306c0
Andnrew Cheon <cheonandrew113@gmail.com>
2019-06-23 05:45:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,7 @@ def second_number >= 0
   
   sum = first_number + second_number
   difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number/second_number
 end
 end
